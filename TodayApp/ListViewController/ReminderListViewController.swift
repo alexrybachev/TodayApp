d8ea1b2010e3fe7f974b6034676ff88fd91e60bb
Alex Rybachev <rybachev.as@gmail.com>
2023-03-05 12:30:25 +0400

    Displaying cell info

--- TodayApp/ListViewController/ReminderListViewController.swift
+++ TodayApp/ListViewController/ReminderListViewController.swift
@@ -8,23 +8,24 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
-    
+   
     var dataSource: DataSource!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
         
-        let cellRegistration = UICollectionView.CellRegistration {
-            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
-            let reminder = Reminder.sampleData[indexPath.item]
-            var contentConfiguration = cell.defaultContentConfiguration()
-            contentConfiguration.text = reminder.title
-            cell.contentConfiguration = contentConfiguration
-        }
+//        let cellRegistration = UICollectionView.CellRegistration {
+//            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+//            let reminder = Reminder.sampleData[indexPath.item]
+//            var contentConfiguration = cell.defaultContentConfiguration()
+//            contentConfiguration.text = reminder.title
+//            cell.contentConfiguration = contentConfiguration
+//        }
+        
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
         
         dataSource = DataSource(collectionView: collectionView) {
             (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
