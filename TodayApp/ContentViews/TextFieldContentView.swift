ffff061ab7db24f38c47064840d56b4f23990951
Alex Rybachev <rybachev.as@gmail.com>
2023-03-07 17:57:39 +0400

    Adding and deleting reminders

--- TodayApp/ContentViews/TextFieldContentView.swift
+++ TodayApp/ContentViews/TextFieldContentView.swift
@@ -35,7 +35,7 @@ class TextFieldContentView: UIView, UIContentView {
         self.configuration = configuration
         super.init(frame: .zero)
         addPinnedSubview(textField, insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
-        textField.addTarget(self, action: #selector(didChange), for: .editingChanged)
+        textField.addTarget(self, action: #selector(didChange(_:)), for: .editingChanged)
         textField.clearButtonMode = .whileEditing
     }
     
