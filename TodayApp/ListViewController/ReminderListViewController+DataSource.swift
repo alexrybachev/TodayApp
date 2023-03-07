834e2262cf4187b415c86f10600ebcb5b2e76e7a
Alex Rybachev <rybachev.as@gmail.com>
2023-03-08 00:51:55 +0400

    Loading reminders

--- TodayApp/ListViewController/ReminderListViewController+DataSource.swift
+++ TodayApp/ListViewController/ReminderListViewController+DataSource.swift
@@ -20,6 +20,8 @@ extension ReminderListViewController {
         NSLocalizedString("Not completed", comment: "Reminder not completed value")
     }
     
+    private var reminderStore: ReminderStore { ReminderStore.shared }
+    
     func updateSnapshot(reloading idsThatChanged: [Reminder.ID] = []) {
         let ids = idsThatChanged.filter { id in filteredReminders.contains(where: {$0.id == id}) }
         var snapshot = Snapshot()
@@ -70,15 +72,6 @@ extension ReminderListViewController {
         updateSnapshot(reloading: [id])
     }
     
-    private func doneButtonAccessibilityAction(for reminder: Reminder) -> UIAccessibilityCustomAction {
-        let name = NSLocalizedString("Toggle completion", comment: "Reminder done button accessibility label")
-        let action = UIAccessibilityCustomAction(name: name) { [weak self] action in
-            self?.completeReminder(withId: reminder.id)
-            return true
-        }
-        return action
-    }
-    
     func addReminder(_ reminder: Reminder) {
         reminders.append(reminder)
     }
@@ -86,6 +79,32 @@ extension ReminderListViewController {
     func deleteReminder(withId id: Reminder.ID) {
         let index = reminders.indexOfReminder(withId: id)
         reminders.remove(at: index)
+    }
+    
+    func prepareReminderStore() {
+        Task {
+            do {
+                try await reminderStore.requestAccess()
+                reminders = try await reminderStore.readAll()
+            } catch TodayError.accessDenied, TodayError.accessRestricted {
+                #if DEBUG
+                reminders = Reminder.sampleData
+                #endif
+            } catch {
+                showError(error)
+            }
+            
+            updateSnapshot()
+        }
+    }
+    
+    private func doneButtonAccessibilityAction(for reminder: Reminder) -> UIAccessibilityCustomAction {
+        let name = NSLocalizedString("Toggle completion", comment: "Reminder done button accessibility label")
+        let action = UIAccessibilityCustomAction(name: name) { [weak self] action in
+            self?.completeReminder(withId: reminder.id)
+            return true
+        }
+        return action
     }
     
     private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
